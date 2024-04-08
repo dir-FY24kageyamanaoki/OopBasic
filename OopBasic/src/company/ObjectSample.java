0bb5a51124c4bf201178b99ad89ede691d83491b
dir-FY24kageyamanaoki <naoki.kageyama.c382@dirbato.co.jp>
2024-04-08 17:20:35 +0900

    レッスン4.4

--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -18,13 +18,44 @@ public class ObjectSample {
         
         //インスタンスの作成
         var devDepartment = new Department("開発部", "yy", 0);
-        var engineer = new Employee("吉金", devDepartment, "一般社員", 88);
+        var engineer = new Engineer("吉金", devDepartment, "一般社員", 88, "Java");//変更
         
         //インスタンスメソッドの呼び出し
         engineer.report();
         engineer.joinMeeting(
                 );
+        engineer.developSoftware();  //追記
+        
+        
+        
+        //追記
+        //ポリモーフィズムの確認
+        Employee projectManager = new Engineer("村井", devDepartment, "PM", 99, "Python");
+        
+        //インスタンスメソッドの呼び出し
+        projectManager.report();
+        projectManager.joinMeeting();
+        if (projectManager instanceof Engineer) {
+            //developSoftwareメソッドを呼び出す
+            ((Engineer) projectManager).developSoftware();
+           }
+        
+        
+        //1行空ける
+        System.out.println("");
+        
+        //アルバイトインスタンスの作成
+        var parttimeWorker = new ParttimeWorker("田中", department);
+        
+        //実装したメソッドの呼び出し
+        ((Workable) employee).work();
+        ((Workable) engineer).work();
+        ((Workable) projectManager).work();
+        ((Workable) parttimeWorker).work();
         
     }
-
+    
 }
+        
+        
+            
